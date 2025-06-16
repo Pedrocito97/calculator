1dda250dfc7180ffb0d9e044854918f4d1cabb21
Pedrocito97 <pierre.willemart.110497@gmail.com>
2025-06-16 22:00:16 +0200

    Update calculator components and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <div>
       <Header />
+      <UserInput />
     </div>
   )
 }
