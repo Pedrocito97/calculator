c74f9bd128f271ed6bea6aee5fd0d0fd0e08f0e7
Pedrocito97 <pierre.willemart.110497@gmail.com>
2025-06-18 17:36:52 +0200

    Complete Investment Calculator app with Results component and validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import Header from "./components/header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 100000,
+    annualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10,
+});
+
+function handleChange(inputIdentifier, newValue) {
+  setUserInput((prevInput) => {
+      return {
+          ...prevInput,
+          [inputIdentifier]: +newValue,
+      }
+  })
+}
+
+const inputIsValid = userInput.duration >= 1;
+
   return (
-    <div>
+    <>
       <Header />
-      <UserInput />
-    </div>
+      <UserInput onChange={handleChange} userInput={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a valid duration (at least 1 year).</p>}
+      {inputIsValid && <Results input={userInput}/>}
+    </>
   )
 }
 
